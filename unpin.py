295f72094b966f1570eb9866fe3228bbf017d1ea
catx0rr <catx0rr@gmail.com>
2020-05-26 16:48:15 +0800

    initial commit

---
+++ unpin.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+'''
+    unpin.py -- Simple 4-digit bruteforcer
+
+    Note:
+        Modify the script for bruteforcing can be used as password list for 4-digit pins
+'''
+
+from itertools import product
+
+
+filename = 'pin.txt'
+
+def generate_pin():
+
+    with open(filename, 'w') as pinfile:
+
+        for i in product(range(10), repeat=4):
+            pinfile.write('%s\n' % ''.join(map(str, i)))
+
+        pinfile.close()
+
+
+if __name__ == '__main__':
+    generate_pin()
